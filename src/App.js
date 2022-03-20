305ad99c73cec0b8ba88ace9637d96ea3b4b7b19
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-03-20 12:47:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Person name="rubel " nayika="Tapu" phonenumber='01777' location="India"></Person>
+    <Person name="bappa " nayika="Tanu" phonenumber='01777' location="India"></Person>
+      <Person name="bappida " nayika="Tanusree Saha" phonenumber='01777' location="India"></Person>
+      <Name2  name="rubela" ></Name2>
+    </div>)
 }
+
+function Person(props) {
+  console.log(props)
+
+      return (
+        <div className='person'>
+          <h1> Name:{props.name} </h1>
+          <p>Nayika: { props.nayika}</p>
+          <p>Phone Number: { props.phonenumber}</p>
+          <p>Location: { props.location}</p>
+        </div>
+      )
+      
+    }
+  
+function Name2(props) {
+  console.log(props)
+      
+  return (
+    <div className="person2">
+      <h1>Ajay Devgan</h1>
+      <p>Profession:Maramari</p>
+    </div>
+  )
+      
+    }
+
 
 export default App;
